b109936f3b33408c2e67ae3e422381e6f02349dd
chaoshuai <chaoshuai@takecloud.cn>
2018-07-08 13:58:37 +0800

    目录结构

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Test from './pages/test';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Test></Test>
+      </div>
+    );
+  }
+}
+
+export default App;
